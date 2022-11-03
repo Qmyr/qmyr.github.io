3313123c176b13724b2977f844ec9c3163459311
Qmyr <107112206+Qmyr@users.noreply.github.com>
2022-11-03 16:08:09 +0800

    Delete AppSource/TVBox/Source/JS/lib directory

--- AppSource/TVBox/Source/JS/lib/drpy.ym.js
+++
@@ -1,4 +0,0 @@
-import 'https://jsd.cdn.zzko.cn/gh/Qmyr/qmyr.github.io@main/AppSource/TVBox/Source/JS/lib/util.ym.js'
-import dr from 'https://jsd.cdn.zzko.cn/gh/Qmyr/qmyr.github.io@main/AppSource/TVBox/Source/JS/lib/drpy.min.js'
- 
-__JS_SPIDER__ = dr.DRPY()
